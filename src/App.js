9a9a3faed68b875104a0e6f8c7bcba54c55930a8
Larissa Araújo <lariaraujo201@gmail.com>
2020-04-19 10:19:03 -0300

    chore: some minor updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-import Pokedex from './components/Pokedex';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Pokedex />
     </>
   );
 }
